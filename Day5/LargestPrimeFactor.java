0eacf30ae9e9fe544b7b1057c802b40877211a37
Akbar Khan <akbarkhan011750@gmail.com>
2026-06-14 19:50:54 +0530

    Add files via upload

---
+++ Day5/LargestPrimeFactor.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class LargestPrimeFactor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        long n = sc.nextLong();
+
+        long largestPrimeFactor = 1;
+
+        for (long i = 2; i <= n; i++) {
+            while (n % i == 0) {
+                largestPrimeFactor = i;
+                n /= i;
+            }
+        }
+
+        System.out.println("Largest Prime Factor = " + largestPrimeFactor);
+
+    }
+}
