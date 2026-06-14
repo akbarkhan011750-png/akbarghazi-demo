1d1a21201ddf0654310af23049e57e17ca1a353f
Akbar Khan <akbarkhan011750@gmail.com>
2026-06-14 19:53:59 +0530

    Add files via upload

---
+++ Day7/RecursiveSumOfDigit.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class RecursiveSumOfDigit {
+
+    static int sumOfDigits(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        return (n % 10) + sumOfDigits(n / 10);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        int result = sumOfDigits(n);
+
+        System.out.println("Sum of digits = " + result);
+
+    }
+}
