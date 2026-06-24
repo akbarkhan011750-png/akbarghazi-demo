f2c0d0616375ee5bf5c4a87d009dc83bff378f93
Akbar Khan <akbarkhan011750@gmail.com>
2026-06-24 21:47:29 +0530

    Add files via upload

---
+++ Day23/AnagramString.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+class AnagramString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first string: ");
+        String str1 = sc.nextLine();
+
+        System.out.print("Enter second string: ");
+        String str2 = sc.nextLine();
+
+        char[] a = str1.toLowerCase().toCharArray();
+        char[] b = str2.toLowerCase().toCharArray();
+
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        if (Arrays.equals(a, b))
+            System.out.println("Anagram Strings");
+        else
+            System.out.println("Not Anagram Strings");
+    }
+}
