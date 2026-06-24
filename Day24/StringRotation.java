39494f45461253a880124a527abe224330d84dad
Akbar Khan <akbarkhan011750@gmail.com>
2026-06-24 21:48:48 +0530

    Add files via upload

---
+++ Day24/StringRotation.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class StringRotation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first string: ");
+        String str1 = sc.nextLine();
+
+        System.out.print("Enter second string: ");
+        String str2 = sc.nextLine();
+
+        if (str1.length() == str2.length() &&
+            (str1 + str1).contains(str2)) {
+            System.out.println("String is Rotation");
+        } else {
+            System.out.println("String is Not Rotation");
+        }
+    }
+}
